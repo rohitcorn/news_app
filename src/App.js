8a1f329d7d2a7133563cf19d90be5cb294da63f1
Rohit Raj <rohitraj.kumawat@gmail.com>
2024-09-28 22:17:39 +0530

    news second commit

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ function App() {
   const [newsPage, setNewsPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   
-  const api = `https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=20&apiKey=${process.env.REACT_APP_MY_API_KEY}&page=${newsPage}`;
+  // const api = `https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=20&apiKey=${process.env.REACT_APP_MY_API_KEY}&page=${newsPage}`;
+  const api = `https://newsapi.org/v2/everything?q=apple&from=2024-09-27&to=2024-09-27&sortBy=popularity&pageSize=20&apiKey=${process.env.REACT_APP_MY_API_KEY}&page=${newsPage}`;
 
   const newsAPI = async () => {
     setIsLoading(true);
     try{
-      const response = await fetch(api);
+      const response = await fetch(api,{
+        method: "GET",
+        // headers: {"Content-type": "application/json;charset=UTF-8"}
+      });
       const data = await response.json();
       setNewsList(data.articles);
       setFilteredNews(data.articles);
